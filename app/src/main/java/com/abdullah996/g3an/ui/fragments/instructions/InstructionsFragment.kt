6f358551893bea459c6fd4005007aeabbf8d276c
abdullah <abdullaheladly@gmail.com>
2021-11-14 16:28:59 +0200

    add webView for instructions Fragment

--- app/src/main/java/com/abdullah996/g3an/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/abdullah996/g3an/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.abdullah996.g3an.R
+import com.abdullah996.g3an.model.Result
+import com.abdullah996.g3an.util.Constants
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
 
@@ -15,8 +19,18 @@ class InstructionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val view =inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        view.instructions_webView.webViewClient=object :WebViewClient(){}
+        val websiteUrl:String=myBundle!!.sourceUrl
+        view.instructions_webView.loadUrl(websiteUrl)
+        return view
     }
+
+
 
 
 }
